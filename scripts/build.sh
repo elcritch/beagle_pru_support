94c9c4414d1ba96b72311dd105447a1d31c1c5a6
Jaremy Creechley <creechley@gmail.com>
2018-06-15 01:24:18 -0600

    Updates

---
+++ scripts/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Example
+#
+# archive.sh /path/to/build_dir /path/to/artifact.tar.xz
+
+
+set -e
+set -x
+
+WORK_DIR=$1
+TARBALL_PATH=$2
+
+INSTALL_DIR=_build/pru/
+
+BUILD_OS=$(uname -s)
+
+if [[ $BUILD_OS = "Darwin" ]]; then
+    # Use GNU tar from Homebrew (brew install gnu-tar)
+    TAR=gtar
+else
+    TAR=tar
+fi
+
+
+# Assemble the tarball for the toolchain
+# TARGET_TUPLE=$INSTALL_DIR
+# TAR_PATH="${TARBALL_PATH}"
+# TOOLCHAIN_BASE_NAME="$(basename ${TARBALL_PATH%.*.*})/"
+
+MIX_ENV=test PRU_CGT=/dev/null make clean
+make artifact
+
+# Copy build files
+mkdir -p $WORK_DIR/$INSTALL_DIR
+cp -R $INSTALL_DIR/ $WORK_DIR/$INSTALL_DIR
+
+
