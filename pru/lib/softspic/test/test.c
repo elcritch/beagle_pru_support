77a72c1bb41218a10bbbf40d8c201064438a3193
Jaremy Creechley <creechley@gmail.com>
2018-08-06 15:42:06 -0600

    Updates

--- pru/lib/softspic/test/test.c
+++ pru/lib/softspic/test/test.c
@@ -21,3 +21,28 @@ int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
     return munit_suite_main(&test_suite, (void*) "µnit", argc, argv);
 }
 
+
+#include "unity.h"
+#include "DumbExample.h"
+
+void test_AverageThreeBytes_should_AverageMidRangeValues(void)
+{
+  TEST_ASSERT_EQUAL_HEX8(40, AverageThreeBytes(30, 40, 50));
+  TEST_ASSERT_EQUAL_HEX8(40, AverageThreeBytes(10, 70, 40));
+  TEST_ASSERT_EQUAL_HEX8(33, AverageThreeBytes(33, 33, 33));
+}
+
+void test_AverageThreeBytes_should_AverageHighValues(void)
+{
+  TEST_ASSERT_EQUAL_HEX8(80, AverageThreeBytes(70, 80, 90));
+  TEST_ASSERT_EQUAL_HEX8(127, AverageThreeBytes(127, 127, 127));
+  TEST_ASSERT_EQUAL_HEX8(84, AverageThreeBytes(0, 126, 126));
+}
+
+int main(void)
+{
+  UNITY_BEGIN();
+  RUN_TEST(test_AverageThreeBytes_should_AverageMidRangeValues);
+  RUN_TEST(test_AverageThreeBytes_should_AverageHighValues);
+  return UNITY_END();
+}
